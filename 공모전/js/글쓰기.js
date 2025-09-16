88be6d67584aa56a97443114e9a2f9deac3ed7e9
shinjisoo <white_kite08@naver.com>
2025-09-17 03:53:13 +0900

    글 작성 가능

--- 공모전/js/글쓰기.js
+++ 공모전/js/글쓰기.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const title = document.getElementById('title').value;
     const content = document.getElementById('content').value;
-    const username = localStorage.getItem('username');
+    const username = localStorage.getItem('username'); // 로그인 시 저장된 유저 이름
 
     if (!username) {
       alert("로그인 후 글 작성이 가능합니다.");
@@ -25,6 +25,8 @@ document.addEventListener('DOMContentLoaded', function () {
     localStorage.setItem('posts', JSON.stringify(posts));
 
     alert('글이 등록되었습니다.');
+
+    // 프로필로 이동
     window.location.href = '프로필.html';
   });
 });
